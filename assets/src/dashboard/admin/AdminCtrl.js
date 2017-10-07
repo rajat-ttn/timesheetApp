8320f68e2a3ee78984be7a8fa2eaebb02bf4ead2
Anand Nath Shukla <anandnath.shukla@tothenew.com>
2017-10-07 06:52:11 +0530

    Fix Api

--- assets/src/dashboard/admin/AdminCtrl.js
+++ assets/src/dashboard/admin/AdminCtrl.js
@@ -1,11 +1,8 @@
 (function () {
-    angular.module("timeSheet").controller('AdminCtrl', ['$scope', 'isUser', 'ApiService', 'UserService', '$state', function ($scope, isUser, ApiService, UserService, $state) {
+    angular.module("timeSheet").controller('AdminCtrl', ['$scope', 'User', 'ApiService', 'UserService', '$state', function ($scope, User, ApiService, UserService, $state) {
         var vm = this;
         vm.user = isUser;
 
-        /*vm.logout = function () {
-            UserService.logout();
-            $state.go('home.login')
-        };*/
+
     }]);
 })();
