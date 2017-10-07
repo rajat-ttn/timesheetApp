07236b583ea0692b252c961fb02d3e7f179a4d7e
Anand Nath Shukla <anandnath.shukla@tothenew.com>
2017-10-08 00:10:30 +0530

    Fix sheet download

--- assets/src/states/sheet/sheet.js
+++ assets/src/states/sheet/sheet.js
@@ -28,7 +28,7 @@ class SheetCtrl {
         this.files = [];
         if (this.filter.region && this.filter.month && this.filter.year) {
             this.regions[this.filter.region].forEach(project => {
-                this.files.push(`${this.filter.region}_${project.projectName}_${this.filter.month}_${this.filter.year}.xlsx`);
+                this.files.push(`${this.filter.region}_${project.projectName}_${this.filter.month}_${this.filter.year}`);
             });
         }
     }
